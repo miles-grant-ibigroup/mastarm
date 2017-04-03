1dc1dcc02906a003a4932bf5bf591a2027ad7863
Trevor Gerhardt <trevorgerhardt@gmail.com>
2017-04-03 17:01:07 +0200

    test(logger): Remove create logger

--- __tests__/lib/push-to-s3.js
+++ __tests__/lib/push-to-s3.js
@@ -8,14 +8,12 @@ const MOCK_DIR = path.join(process.cwd(), '__tests__/test-utils/mocks')
 describe('lib > push to s3', () => {
   const configPush = require('../../lib/push-to-s3')
   const loadConfig = require('../../lib/load-config')
-  const createLogger = require('../../lib/logger')
 
   it('should compile JavaScript and CSS and send to s3 via aws-sdk', () => {
     const config = loadConfig(process.cwd(), 'configurations/default', 'development')
     const push = configPush({
       env: 'development',
       config,
-      log: createLogger(),
       minify: false,
       s3bucket: 'test-bucket'
     })
