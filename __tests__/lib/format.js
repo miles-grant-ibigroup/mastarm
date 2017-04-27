7aa9732ee322e5495c83e9fd65c8c85cda483cea
Evan Siroky <evan.siroky@yahoo.com>
2017-04-27 11:02:38 -0700

    refactor(format, lint-messages): use async fs api

--- __tests__/lib/format.js
+++ __tests__/lib/format.js
@@ -10,8 +10,10 @@ describe('format', () => {
   beforeEach(done => fs.writeFile('test.js', 'function a(){return 1;}', done))
   afterEach(done => rimraf('test.js', done))
 
-  it('can format a file', () => {
-    format(['test.js'])
-    expect(fs.readFileSync('test.js', {encoding: 'utf-8'})).toMatchSnapshot()
+  it('can format a file', done => {
+    format(['test.js']).then(() => {
+      expect(fs.readFileSync('test.js', {encoding: 'utf-8'})).toMatchSnapshot()
+      done()
+    })
   })
 })
