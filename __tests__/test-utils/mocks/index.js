bee4a46a3435326e757bfc2d610f8e587098764c
Evan Siroky <evan.siroky@yahoo.com>
2017-09-28 15:30:02 -0700

    docs(all): add needed docstrings to pass lint

--- __tests__/test-utils/mocks/index.js
+++ __tests__/test-utils/mocks/index.js
@@ -1,11 +1,15 @@
 import uuid from 'uuid'
 import png from '../../../mastarm.png'
 
+/** A test component */
 export default class MockTestComponentUniqueName {
   static defaultProps = {
     test: 'hi'
   }
 
+  /**
+   * Render the component.
+   */
   render () {
     return <div />
   }
